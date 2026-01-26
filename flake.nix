c853faa173afc441b1d7265e046ffc4e9f0cfe68
Paradox <paradox@lenovonix>
2026-01-26 20:44:22 +0100

    Update flake.nix with specialArgs

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "NixOS configuration for Lenovo E14 Gen 5";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    nixosConfigurations.lenovonix = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/lenovonix/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.paradox = import ./home-manager/paradox.nix;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+        }
+      ];
+    };
+
+    homeConfigurations.paradox = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [ ./home-manager/paradox.nix ];
+      extraSpecialArgs = { inherit inputs; };
+    };
+  };
+}
---
+++ home-manager/paradox.nix
@@ -0,0 +1,92 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  home.username = "paradox";
+  home.homeDirectory = "/home/paradox";
+  home.stateVersion = "25.11";
+
+  # Packages to install for the user
+  home.packages = with pkgs; [
+    neovim
+    tmux
+    fish
+    bash
+    # ghostty terminal - not sure if available in nixpkgs, let's search later
+    # zed-editor - already installed system-wide, maybe we want via home-manager
+    zed-editor
+    git
+    # Additional tools
+    htop
+    ripgrep
+    fd
+    exa
+    bat
+  ];
+
+  # Git configuration
+  programs.git = {
+    enable = true;
+    userName = "Paradox";
+    userEmail = "your-email@example.com"; # Please update this
+    extraConfig = {
+      init.defaultBranch = "main";
+      pull.rebase = true;
+    };
+  };
+
+  # Shell configuration (bash)
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      ll = "exa -la";
+      gs = "git status";
+      gcm = "git commit -m";
+    };
+  };
+
+  # Fish shell configuration
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      ll = "exa -la";
+      gs = "git status";
+    };
+    # Set fish as default shell if desired
+    # shellInit = ''
+    #   set -g fish_greeting ""
+    # '';
+  };
+
+  # Neovim configuration (basic)
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    defaultEditor = true;
+    plugins = with pkgs.vimPlugins; [
+      vim-nix
+      vim-commentary
+    ];
+    extraConfig = ''
+      set number
+      set relativenumber
+      set tabstop=2
+      set shiftwidth=2
+      set expandtab
+    '';
+  };
+
+  # Tmux configuration
+  programs.tmux = {
+    enable = true;
+    keyMode = "vi";
+    terminal = "screen-256color";
+    extraConfig = ''
+      set-option -g mouse on
+      set-option -g status-style bg=black
+    '';
+  };
+
+  # Let home-manager manage its own state
+  programs.home-manager.enable = true;
+}
