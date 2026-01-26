88e09fe184e387d5eef898bce4ef35d1ecfcf7c6
Paradox <paradox@lenovonix>
2026-01-26 21:20:28 +0100

    Use nixpkgs-unstable for opencode to get newer bun

--- flake.nix
+++ flake.nix
@@ -3,13 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/master";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     opencode = {
       url = "github:anomalyco/opencode";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
 
